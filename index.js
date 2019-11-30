3c7b58c308ffce25d42dfc3d494c9fc6e849d6e0
zwe_home <scott13.zwe@gmail.com>
2019-11-30 16:12:27 +0800

    Testing Node JS

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-//my testing
-TESTING
-123
+//Tutorial - 1
+/* const tutorial = require('./tutorial');
+console.log(tutorial);
+console.log(tutorial.sum(3,7));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject()); */
+
+
+//Tutorial - 2
+const EventEmitter = require('events');
+const ee = new EventEmitter();
+
+ee.on('tutorial2', (num1, num2) => {
+    console.log('The number is : ' + (num1 + num2));
+})
+
+ee.emit('tutorial2', 1, 2);
